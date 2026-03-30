433a3ceffd0f9c8821f6be2ed945dcd11adbc5d0
EzraZRNaid00 <165680709+EzraZRNaid00@users.noreply.github.com>
2026-03-30 22:10:16 +0200

    Created the Intent class with basic properties tried to create basic program.cs

--- Intent.cs
+++ Intent.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace CybersecurityChatbot
 {
-    internal class Intent
+    public class Intent
     {
+        public string Name { get; set; }
+        public List<string> Keywords { get; set; }
+        public List<string> Responses { get; set; }
+
+        public Intent()
+        {
+            Keywords = new List<string>();
+            Responses = new List<string>();
+        }
+
+        public Intent(string name, List<string> keywords, List<string> responses)
+        {
+            Name = name;
+            Keywords = keywords;
+            Responses = responses;
+        }
+
+        public bool Matches(string input)
+        {
+            string lowerInput = input.ToLower();
+            return Keywords.Any(keyword => lowerInput.Contains(keyword.ToLower()));
+        }
+
+        public string GetResponse()
+        {
+            if (Responses == null || Responses.Count == 0)
+                return "I am unable to give you a valid response";
+
+            Random random = new Random();
+            int index = random.Next(Responses.Count);
+            return Responses[index];
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-   
+using System;
+
+namespace CybersecurityChatbot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Cybersecurity Awareness Bot";
+
+            // Get user name
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(userName))
+            {
+                userName = "User";
+            }
+
+            // Capitalize first letter
+            userName = char.ToUpper(userName[0]) + userName.Substring(1).ToLower();
+
+
+        }
+    }
+}
